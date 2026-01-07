6c415d685dc2a80f0d816c798950c831dd5ee170
IceLing <lemonadorable@gmail.com>
2026-01-07 22:27:31 +0800

    fmt

--- contracts/TopicBountyEscrow.sol
+++ contracts/TopicBountyEscrow.sol
@@ -8,13 +8,7 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract TopicBountyEscrow is
-    Initializable,
-    OwnableUpgradeable,
-    PausableUpgradeable,
-    ReentrancyGuard,
-    UUPSUpgradeable
-{
+contract TopicBountyEscrow is Initializable, OwnableUpgradeable, PausableUpgradeable, ReentrancyGuard, UUPSUpgradeable {
     enum ProposalStatus {
         Created,
         Voting,
